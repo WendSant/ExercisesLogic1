88f9defc32b56f51b8da163a1992d39186e7afe0
Wendson <wendsonsantanajps@gmail.com>
2021-08-09 22:48:03 -0300

    Exercises programming logic

---
+++ script.js
@@ -0,0 +1,35 @@
+// Exercicio 1
+// alert("Olá, mundo!");
+
+// Exercicio 2
+// let nameUser = prompt("Qual seu nome?");
+// alert("Seja bem vindo, " + nameUser + " !");
+
+// Exercicio 3
+// let nameUser = prompt("Digite seu nome: ");
+// let salario = prompt("Digite seu salario (Ex: 1000,00) : ");
+
+// alert("Nome do funcionário(a) : "+ nameUser + "\nSalário : R$"+  salario +" \nO(a) funcionário(a) "+ nameUser +" tem um salário de R$"+ salario +" em Junho." );
+
+// Exercicio 4
+// let numero1 = parseInt(prompt("Digite o primeiro numero"));
+// let numero2 = parseInt(prompt("Digite o segundo numero"));
+// let soma = (numero1+numero2);
+// alert('A soma entre os dois números é ' + soma);
+
+// Exercicio 5
+// let nota1 = parseInt(prompt('Digite a primeira nota'));
+// let nota2 = parseInt(prompt('Digite a segunda nota'));
+// alert('Sua média é '+ (nota1+nota2)/2);
+
+// Exericio 6
+
+// let numero = parseInt(prompt('Digite o número'));
+// alert('Número digitado: '+numero+'\n Seu antecessor é  '+(numero-1)  +'\n Seu sucessor é '+(numero+1));
+
+// Exercise seven
+// let numero = prompt("Digite o número");
+// alert('Número digitado: '+numero+'\nO dobro desse número é: '+(numero*2)+'\nE sua terça parte é: '+(numero/3));
+
+// Exercise eigth
+
